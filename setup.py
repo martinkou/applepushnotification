716474168fc8854588d2bc3262e66e8fbbdac4b2
Martin Kou <martinkou@tixxme.com>
2011-03-25 19:50:45 -0700

    Fixed typo in package name.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 
 from setuptools import setup, find_packages
 
-setup(name = "applepushnotifcation",
+setup(name = "applepushnotification",
 		version="0.1",
 		packages=["applepushnotification"],
 		author = "Kou Man Tong",
